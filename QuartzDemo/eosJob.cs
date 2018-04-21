e7e91de7727160af3790981f58bdec28b998ccf9
xiongzhenglong <7448688887@qq.com>
2018-04-21 12:57:25 +0800

    ddd

---
+++ QuartzDemo/eosJob.cs
@@ -0,0 +1,169 @@
+﻿using Quartz;
+using RestSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace QuartzDemo
+{
+    [DisallowConcurrentExecution]
+    class eosJob : IJob
+    {
+
+        private static string tag = "eos";
+        private static string origin = "https://ceo.bi";
+        private static string url_index = $"https://ceo.bi/trade/index_json?market={tag}_cny";
+        private static string url_buy = "https://ceo.bi/trade/up.html";
+        private static string url_chexiao = "https://ceo.bi/trade/chexiao.html";
+        private static string refer = $"https://ceo.bi/t/cny_{tag}.jsp";
+
+        private static string url_referprice = "";
+        private static string sessionid = "nkn1qpraha5vcev59mcg21mqg7";
+
+        public CEO getIndex(out string sec_)
+        {
+            var client = new RestClient(url_index);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("cookie", "aliyungf_tc=AQAAABcs4VQ2wwoAzi2M0ufA9uQkjTEh; JSPSESSID=nkn1qpraha5vcev59mcg21mqg7");
+            request.AddHeader("accept-language", "zh-CN,zh;q=0.9");
+            request.AddHeader("accept-encoding", "gzip, deflate, br");
+            request.AddHeader("referer", "https://ceo.bi/t/cny_eos.jsp");
+            request.AddHeader("content-type", "application/x-www-form-urlencoded");
+            request.AddHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
+            request.AddHeader("x-requested-with", "XMLHttpRequest");
+            request.AddHeader("origin", "https://ceo.bi");
+            request.AddHeader("accept", "application/json, text/javascript, */*; q=0.01");
+      
+            IRestResponse response = client.Execute(request);
+            sec_ =response.Cookies!=null&&response.Cookies.Count>0? response.Cookies[0].Value:"";
+            return JsonHelper.DeserializeJsonToObject<CEO>(response.Content);
+        }
+
+        public reprice getReferPrice(string rptag)
+        {
+            var client = new RestClient($"https://api.coinmarketcap.com/v1/ticker/{rptag}/?convert=cny");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("accept-language", "zh-CN,zh;q=0.9");
+            request.AddHeader("accept-encoding", "gzip, deflate, br");
+            request.AddHeader("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+            request.AddHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
+            request.AddHeader("upgrade-insecure-requests", "1");
+            IRestResponse response = client.Execute(request);
+            return JsonHelper.DeserializeJsonToObject<reprice>(response.Content.Substring(1, response.Content.Length - 2));
+        }
+
+        public void buy(string sec_, string num, string price)
+        {
+            var client = new RestClient(url_buy);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("cookie", "aliyungf_tc=AQAAABcs4VQ2wwoAzi2M0ufA9uQkjTEh; JSPSESSID=nkn1qpraha5vcev59mcg21mqg7");
+            request.AddHeader("accept-language", "zh-CN,zh;q=0.9");
+            request.AddHeader("accept-encoding", "gzip, deflate, br");
+            request.AddHeader("referer", refer);
+            request.AddHeader("content-type", "application/x-www-form-urlencoded");
+            request.AddHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
+            request.AddHeader("x-requested-with", "XMLHttpRequest");
+            request.AddHeader("origin", origin);
+            request.AddHeader("accept", "application/json, text/javascript, */*; q=0.01");
+           
+
+           
+      
+      
+            //request.AddHeader("cookie", $"JSPSESSID={sessionid}; sec_={sec_}");
+            
+           
+         
+            request.AddParameter("market", $"{tag}_cny");
+            request.AddParameter("num", num);
+            request.AddParameter("paypassword", "660218");
+            request.AddParameter("price", price);
+            request.AddParameter("type", "1");
+            IRestResponse response = client.Execute(request);
+        }
+
+        public void chexiao(string sec_, int id)
+        {
+            var client = new RestClient(url_chexiao);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("cookie", $"JSPSESSID={sessionid}; sec_={sec_}");
+            request.AddHeader("accept-language", "zh-CN,zh;q=0.9");
+            request.AddHeader("accept-encoding", "gzip, deflate, br");
+            request.AddHeader("referer", refer);
+            request.AddHeader("content-type", "application/x-www-form-urlencoded");
+            request.AddHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
+            request.AddHeader("x-requested-with", "XMLHttpRequest");
+            request.AddHeader("origin", origin);
+            request.AddHeader("accept", "application/json, text/javascript, */*; q=0.01");
+            request.AddParameter("id", id);
+            IRestResponse response = client.Execute(request);
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+
+            try
+            {
+                reprice rp = getReferPrice("eos");
+                double eosprice = double.Parse(rp.price_cny);
+
+                string sec_ = "";
+                var ceo = getIndex(out sec_);
+
+                double maxprice = double.Parse(ceo.depth.b[0][0]);
+                double maxcount = double.Parse(ceo.depth.b[0][1]);
+
+                double minprice = double.Parse(ceo.depth.s[9][0]);
+                double mincount = double.Parse(ceo.depth.s[9][1]);
+                double totalmoney = maxprice * maxcount;
+                double minmoney = minprice * mincount;
+                Console.WriteLine(maxprice + ":" + totalmoney + "----" + minprice + ":" + minmoney);
+                if (ceo.order!=null)
+                {
+                    foreach (var o in ceo.order)
+                    {
+                        if (double.Parse(o.price) - eosprice > 0.1 && o.type == 1)
+                        {
+
+                            chexiao(sec_, o.id);
+
+                        }
+                    }
+                }
+              
+               
+            
+                if (ceo.order != null && ceo.order.Count > 0)
+                {
+                    Console.WriteLine("2:" + totalmoney);
+                }
+                else
+                {
+                        
+                        buy(sec_, "50", eosprice.ToString("0.00"));
+                        Console.WriteLine("1:" + totalmoney);
+                }
+
+
+             
+
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                ;
+            }
+
+
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ QuartzDemo/oiocJob.cs
@@ -0,0 +1,156 @@
+﻿using Quartz;
+using RestSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace QuartzDemo
+{
+    [DisallowConcurrentExecution]
+    class oiocJob:IJob
+    {
+
+        private static string tag = "oioc";
+        private static string origin = "https://ceo.bi";
+        private static string url_index = $"https://ceo.bi/trade/index_json?market={tag}_cny";
+        private static string url_buy = "https://ceo.bi/trade/up.html";
+        private static string url_chexiao = "https://ceo.bi/trade/chexiao.html";
+        private static string refer = $"https://ceo.bi/t/cny_{tag}.jsp";
+
+        private static string url_referprice = "";
+        private static string sessionid = "nkn1qpraha5vcev59mcg21mqg7";
+
+        public CEO getIndex(out string sec_)
+        {
+            var client = new RestClient(url_index);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddCookie("JSPSESSID", sessionid);
+            IRestResponse<CEO> response = client.Execute<CEO>(request);
+            sec_ = response.Cookies[0].Value;
+            return JsonHelper.DeserializeJsonToObject<CEO>(response.Content);
+        }
+
+        public reprice getReferPrice(string rptag)
+        {
+            var client = new RestClient($"https://api.coinmarketcap.com/v1/ticker/{rptag}/?convert=cny");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("accept-language", "zh-CN,zh;q=0.9");
+            request.AddHeader("accept-encoding", "gzip, deflate, br");
+            request.AddHeader("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+            request.AddHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
+            request.AddHeader("upgrade-insecure-requests", "1");
+            IRestResponse response = client.Execute(request);
+            return JsonHelper.DeserializeJsonToObject<reprice>(response.Content.Substring(1, response.Content.Length-2));
+        }
+
+        public void buy(string sec_,string num,string price)
+        {
+            var client = new RestClient(url_buy);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("cookie", $"JSPSESSID={sessionid}; sec_={sec_}");
+            request.AddHeader("accept-language", "zh-CN,zh;q=0.9");
+            request.AddHeader("accept-encoding", "gzip, deflate, br");
+            request.AddHeader("referer", refer);
+            request.AddHeader("content-type", "application/x-www-form-urlencoded");
+            request.AddHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
+            request.AddHeader("x-requested-with", "XMLHttpRequest");
+            request.AddHeader("x-devtools-emulate-network-conditions-client-id", "2687BE9BCA71601DA7A9F346038BC9FC");
+            request.AddHeader("origin", origin);
+            request.AddHeader("accept", "application/json, text/javascript, */*; q=0.01");
+            request.AddParameter("market", $"{tag}_cny");
+            request.AddParameter("num", num);
+            request.AddParameter("paypassword", "660218");
+            request.AddParameter("price", price);
+            request.AddParameter("type", "1");
+            IRestResponse response = client.Execute(request);
+        }
+
+        public void chexiao(string sec_,int id)
+        {
+            var client = new RestClient(url_chexiao);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("cookie", $"JSPSESSID={sessionid}; sec_={sec_}");
+            request.AddHeader("accept-language", "zh-CN,zh;q=0.9");
+            request.AddHeader("accept-encoding", "gzip, deflate, br");
+            request.AddHeader("referer", refer);
+            request.AddHeader("content-type", "application/x-www-form-urlencoded");
+            request.AddHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
+            request.AddHeader("x-requested-with", "XMLHttpRequest");
+            request.AddHeader("origin", origin);
+            request.AddHeader("accept", "application/json, text/javascript, */*; q=0.01");
+            request.AddParameter("id", id);
+            IRestResponse response = client.Execute(request);
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+         
+            try
+            {
+                reprice rp = getReferPrice("eos");
+
+                string sec_ = "";
+                var ceo = getIndex(out sec_);
+
+                double maxprice = double.Parse(ceo.depth.b[0][0]);
+                double maxcount = double.Parse(ceo.depth.b[0][1]);
+
+                double minprice = double.Parse(ceo.depth.s[9][0]);
+                double mincount = double.Parse(ceo.depth.s[9][1]);
+                double totalmoney = maxprice * maxcount;
+                double minmoney = minprice * mincount;
+                Console.WriteLine(maxprice+":"+totalmoney+"----"+minprice+":"+minmoney);
+                if (ceo.order != null && ceo.order.Count > 0)
+                {
+                    foreach (var o in ceo.order)
+                    {
+                        if (double.Parse(o.price) != maxprice)
+                        {
+                            if (o.type == 1)
+                            {
+                                chexiao(sec_, o.id);
+
+                            }
+                        }
+                    }
+                }
+                if (totalmoney> 3000 )
+                {
+                    if (ceo.order != null && ceo.order.Count > 1)
+                    {
+                        Console.WriteLine("2:" + totalmoney);
+                    }
+                    else
+                    {
+                        if (maxprice < 0.00095)
+                        {
+                            buy(sec_, "3000000", maxprice.ToString());
+                            Console.WriteLine("1:" + totalmoney);
+                        }
+                        
+                      
+                    }
+                    
+                   
+                }
+                
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                ;
+            }
+         
+
+            await Task.CompletedTask;
+        }
+    }
+}
